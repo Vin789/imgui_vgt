e7c2ebe6c8549db2e314501e933eb1ad60583aa8
Vincent Pirault <vincent.pirault@gmail.com>
2026-07-20 14:27:19 -0400

    Fix clang -Werror,-Wuninitialized-const-pointer

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -4733,7 +4733,9 @@ bool ImGui::InputTextEx(const char* label, const char* hint, char* buf, int buf_
                 apply_new_text = "";
                 apply_new_text_length = 0;
                 value_changed = true;
-                IMSTB_TEXTEDIT_CHARTYPE empty_string;
+// VGT BEGIN - Fix clang -Werror,-Wuninitialized-const-pointer
+                IMSTB_TEXTEDIT_CHARTYPE empty_string = 0;
+// VGT BEGIN - Fix clang -Werror,-Wuninitialized-const-pointer
                 stb_textedit_replace(state, &state->Stb, &empty_string, 0);
             }
             else if (strcmp(buf, state->InitialTextA.Data) != 0)
